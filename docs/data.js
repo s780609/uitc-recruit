8d8ae83c15e799272efd5a9472273599610e9e3c
HsuTingHuan <s780609@gmail.com>
2023-06-03 15:08:28 +0800

    feat: Complete test

---
+++ docs/data.js
@@ -0,0 +1,9 @@
+const employeeData = [
+  { empNo: "001", name: "許廷煥", birthday: "80/01/01" },
+  { empNo: "002", name: "林芃君", birthday: "90/01/01" },
+  { empNo: "403", name: "林書證", birthday: "70/01/01" },
+  { empNo: "034", name: "王育昇", birthday: "75/01/01" },
+  { empNo: "015", name: "潘彥良", birthday: "80/01/01" },
+];
+
+export { employeeData };
